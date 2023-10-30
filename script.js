2ab0800ed5b335baa8b8cf29f127d95f956fbb33
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-10-30 23:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// function insert_Row() {
-//     //Write your code here
-  
-  
-// }
+
 function insert_Row() {
+    //Write your code here
   let table = document.getElementById("sampleTable");
   let newtable = document.createElement("tr");
   
   let newcell = document.createElement("td");
   newcell.innerText = "New Cell1";
   let newcell2 = document.createElement("td");
-  newcell2.innerText = "New Cell2";
-  newtable.appendChild(newcell);
-  newtable.appendChild(newcell2);
-  table.appendChild(newtable);
+  newcell2.innerText= "New Cell2";
+  newtable.append(newcell,newcell2);
+ table.appendChild(newtable,table);
 }
